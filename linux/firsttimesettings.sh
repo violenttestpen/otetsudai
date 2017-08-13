fe93c6547048ab1ef60ffb2b7d44e25983545a28
Winston Ho <winstonho90@users.noreply.github.com>
2017-08-13 17:50:34 +0800

    firsttimesettings.sh to include disabling of apt recommended packages

--- linux/firsttimesettings.sh
+++ linux/firsttimesettings.sh
@@ -2,21 +2,31 @@
 #remember to use sudo
 #for use in ubuntu-based distros
 
+# Don't persist /var/log
 TMPLOGDIR="tmpfs	/var/log	tmpfs	defaults,noatime,nodiratime,nodev,nosuid,noexec,size=100M	0 0"
 sudo rm -rf /var/log/*
 echo ${TMPLOGDIR} >> /etc/fstab
 
+# Don't persist /tmp
 TMPDIR="tmpfs	/tmp	tmpfs	defaults,noatime,nodiratime,nodev,nosuid,noexec,size=100M	0 0"
 sudo rm -rf /tmp/*
 echo ${TMPDIR} >> /etc/fstab
 
 mount -a
 
+# Make Linux rely less on swap space
 echo "vm.swappiness = 10" >> /etc/sysctl.conf
 sysctl -p
 
+# Remove guest account
 if [ -f /etc/lightdm/lightdm.conf ]; then
 	echo "\nallow-guest=false" >> /etc/lightdm/lightdm.conf
 fi
 
+# Enable default firewall settings
 sudo ufw enable
+
+# Disable recommended packages for apt
+echo 'APT::Install-Recommends "false";' >> /etc/apt/apt-conf.d/99_norecommends
+echo 'APT::AutoRemove::RecommendsImportant "false";' >> /etc/apt/apt-conf.d/99_norecommends
+echo 'APT::AutoRemove::SuggestsImportant "false";' >> /etc/apt/apt-conf.d/99_norecommends
