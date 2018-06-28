cd241d4a1243a76ab5c0d754ffe49e85ceb4523b
Winston Ho <winstonho90@users.noreply.github.com>
2018-06-28 20:26:02 +0800

    + added android folder * added choco tool to windows/firsttimesettings.bat

--- windows/firsttimesettings.bat
+++ windows/firsttimesettings.bat
@@ -1,1 +1,31 @@
-choco install -y 7zip ccleaner deluge docker-toolbox dropbox git GoogleChrome keepass libreoffice notepadplusplus telegram vagrant virtualbox VisualStudioCode Xming
+REM Install chocolatey
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+REM Install my essential packages
+choco install -y 7zip ^
+		ccleaner ^
+		deluge ^
+		dropbox ^
+		GoogleChrome ^
+		keepass ^
+		libreoffice ^
+		telegram ^
+
+REM Install my development tools
+choco install -y docker-toolbox ^
+		git ^
+		notepadplusplus ^
+		vagrant ^
+		virtualbox ^
+		VisualStudioCode ^
+		Xming
+
+REM Install python and my goto libraries
+choco install -y python3
+pip install --no-cache-dir pipdeptree ^
+			requests ^
+			httpie ^
+			virtualenv
+
+REM Install my pentest tools
+REM choco install -y 
